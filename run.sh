a33dc88e8f6f35e2120618f285f40e216805122e
xinzhao <xinzhao@deepglint.com>
2017-05-20 14:35:28 +0800

    update *sh

--- run.sh
+++ run.sh
@@ -1,2 +1,29 @@
-#./analyze_lost.py ./result.txt ./ground_truth/day_out_label.txt ./id_name_map.txt 0.1 0.6
-./analyze_lost.py ./result.txt ./ground_truth/hg_12_label.txt ./id_name_map_hg.txt ./black_list_repo 0.1 0.6
+#./analyze_lost.py <pred_result> <ground_truth_lable> <id_name_map> <black_list_repo_dir> <iou_thresh> <similarity_score_thresh> 
+
+####DG
+#当前VSD
+./analyze_lost.py ./result.txt ./ground_truth/dg/ground_truth/label.txt ./exp_data/dg/id_name_map_hg.txt ./black_list_repo 0.3 0.6
+
+
+# multi_vote 
+#./analyze_lost.py ./result.txt ./ground_truth/dg/ground_truth/label.txt ./exp_data/dg/id_name_map_hg.txt ./black_list_repo 0.3 0.6
+
+# single_vote 
+#./analyze_lost.py ./result.txt ./ground_truth/dg/ground_truth/label.txt ./exp_data/dg/id_name_map_hg.txt ./black_list_repo 0.3 0.6
+
+# 更新tracking 
+#./analyze_lost.py ./result.txt ./ground_truth/dg/ground_truth/label.txt ./exp_data/dg/id_name_map_hg.txt ./black_list_repo 0.3 0.6
+
+#===================================================================
+####haiguan
+# 当前VSD
+./analyze_lost.py ./result.txt ./exp_data/haiguan/ground_truth/label.txt ./exp_data/haiguan/id_name_map.txt ./black_list_repo 0.3 0.6
+
+# multi_vote 
+#./analyze_lost.py ./result.txt ./exp_data/haiguan/ground_truth/label.txt ./exp_data/haiguan/id_name_map.txt ./black_list_repo 0.3 0.6
+
+# single_vote 
+#./analyze_lost.py ./result.txt ./exp_data/haiguan/ground_truth/label.txt ./exp_data/haiguan/id_name_map.txt ./black_list_repo 0.3 0.6
+
+# 更新tracking 
+#./analyze_lost.py ./result.txt ./exp_data/haiguan/ground_truth/label.txt ./exp_data/haiguan/id_name_map.txt ./black_list_repo 0.3 0.6
--- run_view_log.sh
+++ run_view_log.sh
@@ -1,3 +1,21 @@
-./view_log.py ./dataset/haiguan/12fps ./log/true_alarm.log ./tmp_result/
-./view_log.py ./dataset/haiguan/12fps ./log/misidentify.log ./tmp_result/
-./view_log.py ./dataset/haiguan/12fps ./log/empty_hit.log ./tmp_result/
+#=======================================================
+####haiguan
+#当前VSD
+./view_log.py ./dataset/haiguan/12fps ./log/true_alarm.log ./tmp_result/current
+./view_log.py ./dataset/haiguan/12fps ./log/misidentify.log ./tmp_result/current
+./view_log.py ./dataset/haiguan/12fps ./log/empty_hit.log ./tmp_result/current
+
+#multi_vote
+#./view_log.py ./dataset/haiguan/12fps ./log/true_alarm.log ./tmp_result/multi_vote
+#./view_log.py ./dataset/haiguan/12fps ./log/misidentify.log ./tmp_result/multi_vote
+#./view_log.py ./dataset/haiguan/12fps ./log/empty_hit.log ./tmp_result/multi_vote
+
+#single_vote
+#./view_log.py ./dataset/haiguan/12fps ./log/true_alarm.log ./tmp_result/single_vote
+#./view_log.py ./dataset/haiguan/12fps ./log/misidentify.log ./tmp_result/single_vote
+#./view_log.py ./dataset/haiguan/12fps ./log/empty_hit.log ./tmp_result/single_vote
+
+#update track
+#./view_log.py ./dataset/haiguan/12fps ./log/true_alarm.log ./tmp_result/update_track
+#./view_log.py ./dataset/haiguan/12fps ./log/misidentify.log ./tmp_result/update_track
+#./view_log.py ./dataset/haiguan/12fps ./log/empty_hit.log ./tmp_result/update_track
