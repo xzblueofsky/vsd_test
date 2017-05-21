9c3581fd51a829843b560426d19d301b28440cd0
xinzhao <xinzhao@deepglint.com>
2017-05-21 14:36:17 +0800

    commit

--- run_view_ground_truth.sh
+++ run_view_ground_truth.sh
@@ -1,6 +1,10 @@
 #./view_ground_truth.py ./dataset/dg/12fps ./exp_data/dg/ground_truth/label.txt
 #./view_ground_truth.py ./dataset/haiguan/12fps ./exp_data/haiguan/ground_truth/label.txt
 #./view_ground_truth.py ./dataset/dalian1 ./exp_data/dalian1/ground_truth/dalian_1_12_label.txt
-./view_ground_truth.py ./dataset/dalian1 ./exp_data/dalian1/ground_truth/label.txt
+#./view_ground_truth.py ./dataset/dalian1 ./exp_data/dalian1/ground_truth/label.txt
+
+#./view_ground_truth.py ./dataset/dalian2 ./exp_data/dalian2/ground_truth/dalian_2_12_label.txt
 #./view_ground_truth.py ./dataset/dalian2 ./exp_data/dalian2/ground_truth/label.txt
-#./view_ground_truth.py ./dataset/dalian3 ./exp_data/dalian3/ground_truth/label.txt
+
+#./view_ground_truth.py ./dataset/dalian3 ./exp_data/dalian3/ground_truth/dalian_3_12_label.txt
+./view_ground_truth.py ./dataset/dalian3 ./exp_data/dalian3/ground_truth/label.txt
