19f21203e40ab74d5402d339c581c9fdbac7b270
xinzhao <xinzhao@deepglint.com>
2017-05-21 11:56:15 +0800

    show track id in frame

--- view_ground_truth.py
+++ view_ground_truth.py
@@ -21,6 +21,16 @@ def GetFrameRoiMap(ground_truth_records):
     #print frame_result_dict
     return frame_result_dict
 
+def DrawTaggedROI(frame_image, tag, roi, color):
+    #print roi
+    x = int(float(roi[0]))
+    y = int(float(roi[1])) 
+    w = int(float(roi[2])) 
+    h = int(float(roi[3])) 
+
+    cv2.rectangle(frame_image, (x,y), (x+w, y+h), color, 2)
+    cv2.putText(frame_image, str(tag), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2,(255,255,255),4)
+    return frame_image
 
 if __name__=='__main__':
     print 'main'
@@ -44,9 +54,9 @@ if __name__=='__main__':
         draw_image = frame_image.copy()
         #rois = list()
         for tag in tags:
-            roi = ','.join(x for x in tag[1:])
-            print roi
-            view_log.DrawROI(draw_image, roi, (255, 0, 0))
+            roi = [x for x in tag[1:]]
+            print 'tag = {}, roi={}'.format(tag[0], roi)
+            DrawTaggedROI(draw_image, tag[0], roi, (255, 0, 0))
 
         cv2.putText(draw_image, frame_id, (100,100), cv2.FONT_HERSHEY_SIMPLEX, 4,(255,255,255),4)
         #cv2.imwrite(frame_id + '.jpg', draw_image)
